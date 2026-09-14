ea5058f4ff6e8b9ec792f432b82f8a341810ccdf
KatsiarynaMashko <k.mashko@andersenlab.com>
2026-09-14 14:54:35 +0300

    feat: add router

---
+++ src/pages/home.ts
@@ -0,0 +1,8 @@
+export const initHome = (): string => {
+  return `
+      <div class="u-container">
+        <h1>мини-игры</h1>
+        <p></p>
+      </div>
+    `;
+};
---
+++ src/pages/library.ts
@@ -0,0 +1,7 @@
+export const initLibrary = (): string => {
+  return `
+      <div class="u-container">
+        <h1>library</h1>
+      </div>
+    `;
+};
